d28acfab96989332c424f786016bfa9b5043000e
Basistyi Yevhen <psik726@gmail.com>
2020-10-28 12:53:04 +0100

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Udemy__OOP
+{
+    public abstract class Human
+    {
+        public abstract void SkinColour();
+        public abstract void SayName();
+        public abstract void PlaceLiving();
+        public abstract void Rorar();
+    }
+
+    public class AfroAmerican : Human
+    {
+        private string Skin { get; set; }
+        private string Name { get; set; }
+        private string Place { get; set; }
+
+        public AfroAmerican(string Skin, string Name, string Place)
+        {
+            this.Skin = Skin;
+            this.Name = Name;
+            this.Place = Place;
+        }
+
+        public override void PlaceLiving()
+        {
+            Console.WriteLine($"I am living in {Place}");
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine($"My name id {Name} !");
+        }
+
+        public override void SkinColour()
+        {
+            Console.WriteLine($"My skin is {Skin}");
+        }
+
+        public override void Rorar()
+        {
+            Console.WriteLine("Hi! I am Afroamerican boy!");
+        }
+
+
+    }
+    public class Azian : Human
+    {
+        private string Skin { get; set; }
+        private string Name { get; set; }
+        private string Place { get; set; }
+
+        public Azian(string Skin, string Name, string Place)
+        {
+            this.Skin = Skin;
+            this.Name = Name;
+            this.Place = Place;
+        }
+
+        public override void PlaceLiving()
+        {
+            Console.WriteLine($"I am living in {Place}");
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine($"My name id {Name} !");
+        }
+
+        public override void SkinColour()
+        {
+            Console.WriteLine($"My skin is {Skin}");
+        }
+
+        public override void Rorar()
+        {
+            Console.WriteLine("Hi! I am  Azian");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Udemy__OOP
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Azian Chunhan = new Azian("yellow", "Chunhan", "Pekin");
+            AfroAmerican Billy = new AfroAmerican("Dark-Brown", "Billy", "Egypt");
+
+            Chunhan.SayName();
+            Chunhan.PlaceLiving();
+            Chunhan.SkinColour();
+            Chunhan.Rorar();
+
+            Console.WriteLine();
+
+
+            Billy.SayName();
+            Billy.PlaceLiving();
+            Billy.SkinColour();
+            Billy.Rorar();
+
+            Console.WriteLine();
+
+            Guy billy = new Guy();
+
+            billy.Hello();
+            billy.HowOldYou();
+            billy.SayAll();
+        }
+
+        public class Guy
+        {
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public void Hello()
+            {
+                Console.WriteLine("What is your name?:");
+                this.Name = Console.ReadLine();
+            }
+
+            public void HowOldYou()
+            {
+                Console.WriteLine("What is your age ?:");
+                this.Age = int.Parse(Console.ReadLine());
+            }
+
+            public void SayAll()
+            {
+                Console.WriteLine("Your name is:" + Name);
+                Console.WriteLine("Your age is:" + Age);
+            }
+
+
+        }
+
+    }
+}
